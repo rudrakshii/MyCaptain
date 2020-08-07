eaf921d73d6ab3f078c63e370dd80046ecda8f4c
rudrakshii <69344911+rudrakshii@users.noreply.github.com>
2020-08-07 14:56:42 +0530

    Add files via upload

---
+++ Product of two numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main ()
+{
+    int x,y,product;
+    cout <<"/n Enter two whole numbers";
+    cin>>x>>y;
+    product=x*y;
+    cout<<"/n Product= "<<product;
+    return 0;
+}
