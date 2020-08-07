4ad0201b6374cff0bdbae67f8a738881beba5687
rudrakshii <69344911+rudrakshii@users.noreply.github.com>
2020-08-07 14:50:58 +0530

    Add files via upload

---
+++ Size of characters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main ()
+{
+    char x;
+    int y; float z; double d;
+    cout <<"/n Size of char:"<<sizeof(x)<<endl;
+    cout <<"/n Size of int:"<<sizeof(y)<<endl;
+    cout <<"/n Size of float:"<<sizeof(z)<<endl;
+    cout <<"/n Size of double:"<<sizeof(d)<<endl;
+    return 0;
+}
